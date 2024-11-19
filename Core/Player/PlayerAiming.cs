7156d9c903bac715061cd03d76b96943aad77f2c
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-11-19 13:27:51 +0600

    aim issue

--- Core/Player/PlayerAiming.cs
+++ Core/Player/PlayerAiming.cs
@@ -1,9 +1,1 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerAiming : NetworkBehaviour
-{
-    // Start is called before the first frame update
-    [SerializeField] private Transform turretTransform;
-}
