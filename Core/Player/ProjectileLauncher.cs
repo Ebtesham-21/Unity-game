813420aefc509504e5a72c3d626354a92e1c33f5
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-11-19 18:27:58 +0600

    projectile

---
+++ Core/Player/ProjectileLauncher.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class ProjectileLauncher : NetworkBehaviour
+{
+    
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
