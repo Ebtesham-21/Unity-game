af6ee6294f9c95d052c7d03e37e6a852a7f7d889
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-11-20 15:49:28 +0600

    projectile fire

--- Core/Player/ProjectileLauncher.cs
+++ Core/Player/ProjectileLauncher.cs
@@ -5,11 +5,26 @@ using UnityEngine;
 
 public class ProjectileLauncher : NetworkBehaviour
 {
-    
+    [Header("References")]
+    [SerializeField] private InputReader inputReader;
+    [SerializeField] private Transform projectileSpawnPoint;
+    [SerializeField] private GameObject serverProjectilePrefab;
+    [SerializeField] private GameObject clientProjectilePrefab;
+
+     
+    [Header("Settings")]
+    [SerializeField] private float projectileSpeed;
+
+
+
 
     // Update is called once per frame
     private void Update()
     {
+        
+
+
+
         
     }
 }
