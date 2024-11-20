15504075496ccd2e3fcaafc5639466d67714fa45
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-11-20 16:46:24 +0600

    projectile fire 2

--- Core/Player/ProjectileLauncher.cs
+++ Core/Player/ProjectileLauncher.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
@@ -15,16 +16,48 @@ public class ProjectileLauncher : NetworkBehaviour
     [Header("Settings")]
     [SerializeField] private float projectileSpeed;
 
+    private bool shouldFire;
 
+    public override void OnNetworkSpawn()
+    {
+        if(!IsOwner) {
+            return;
+        }
+
+        inputReader.PrimaryFireEvent += HandlePrimaryFire;
+
+    }
+    public override void OnNetworkDespawn()
+    {
+         if(!IsOwner) {
+            return;
+        }
+
+            inputReader.PrimaryFireEvent -= HandlePrimaryFire;
+    }
 
 
     // Update is called once per frame
     private void Update()
     {
+
+        if(!IsOwner) {return;}
+        if(!shouldFire) {return;}
+
+        SpawnDummyProjectile(projectileSpawnPoint.position, projectileSpawnPoint.up);
+
+
         
+    }
 
+ 
 
+    private void HandlePrimaryFire(bool shouldFire) {
+        this.shouldFire = shouldFire;
+    }
 
-        
+       private void SpawnDummyProjectile(Vector3 spawnPos, Vector3 direction)
+    {
+        Instantiate(clientProjectilePrefab, spawnPos, Quaternion.identity);
     }
 }
