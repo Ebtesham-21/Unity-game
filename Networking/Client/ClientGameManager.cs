2aebb3df827758793fbb5d9d437b0ee9038e8d35
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-11-26 14:00:50 +0600

    network

---
+++ Networking/Client/ClientGameManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Security;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class ClientGameManager 
+{
+    // Start is called before the first frame update
+   public async Task InitAsync()
+   {
+    // AuthenticatedStream player
+    
+   }
+}
