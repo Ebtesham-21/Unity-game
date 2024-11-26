7d2967b764bd7bb5a681f1101c17764519668314
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-11-26 18:56:49 +0600

    authentication

--- Networking/ApplicationController.cs
+++ Networking/ApplicationController.cs
@@ -5,27 +5,33 @@ using UnityEngine;
 
 public class ApplicationController : MonoBehaviour
 {
-    [SerializeField] private ClientSingleton clientPrefab;
-    private async void Start()
+  [SerializeField] private ClientSingleton clientPrefab;
+  [SerializeField] private HostSingleton hostPrefab;
+
+  private async void Start(){
+       DontDestroyOnLoad(gameObject);
+        await  LaunchInMode(SystemInfo.graphicsDeviceType == UnityEngine.Rendering.GraphicsDeviceType.Null);
+
+  }
+
+  private async Task LaunchInMode(bool isDedicatedServer)
+  {
+    if(isDedicatedServer)
     {
-        DontDestroyOnLoad(gameObject);
-      await LaunchInMode (SystemInfo.graphicsDeviceType == UnityEngine.Rendering.GraphicsDeviceType.Null)
-    }
 
-    private async Task LaunchInMode(bool isDedicatedServer)
+    }
+    else 
     {
-        if(isDedicatedServer)
-        {
+     ClientSingleton clientSingleton = Instantiate(clientPrefab);
 
-        }
-        else 
-        {
-          ClientSingleton clientSingleton =   Instantiate(clientPrefab);
+     await clientSingleton.CreateClient();
+     
+     HostSingleton hostSingleton = Instantiate(hostPrefab);
 
-          await clientSingleton.CreateClient();
-
-          //Go to main menu
-        }
+    hostSingleton.CreateHost();
+    //  go to main menu
     }
+  }
+
    
 }
